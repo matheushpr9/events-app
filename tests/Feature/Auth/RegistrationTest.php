56179b8228e7900d20b74f6a53e9624022e751bc
matheushpr9 <matheushpr9@gmail.com>
2025-06-30 15:12:14 -0300

    fix test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -16,5 +16,5 @@ test('new users can register', function () {
     ]);
 
     $this->assertAuthenticated();
-    $response->assertRedirect(route('home', absolute: false));
+    $response->assertRedirect(route('register-space', absolute: false));
 });
