5d217b76e9d0ac14b39a8c068f15b373c998db6c
matheushpr9 <matheushpr9@gmail.com>
2025-08-16 16:11:42 -0300

    fix to live

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-composer install
-npm install
-npm run build
+set -e
+
+# Gera key se não existir
+if [ ! -f /app/.env ]; then
+  cp /app/.env.example /app/.env
+fi
+
+if ! grep -q "APP_KEY=" /app/.env || grep -q "APP_KEY=$" /app/.env; then
+  php artisan key:generate --force
+fi
+
+php artisan migrate
+
 exec "$@"
+
