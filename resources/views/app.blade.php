6e7ee363ca005c98d46385cf30632434dd0f6406
matheushpr9 <matheushpr9@gmail.com>
2025-08-17 23:52:34 -0300

    fix vite

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -40,7 +40,6 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @routes
-        @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
