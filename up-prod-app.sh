bf89995e6cee1104d8265576e2b2ee5e915c1f07
matheushpr9 <matheushpr9@gmail.com>
2025-09-03 11:40:41 -0300

    change sh

--- up-prod-app.sh
+++ up-prod-app.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-rm -rf ./certs/oficialfloov.com.br
-mkdir -p ./certs/oficialfloov.com.br
-chmod -R 777 ./certs
+
+if [[ "$1" == "--certs" ]]; then
+  rm -rf ./certs/oficialfloov.com.br
+  mkdir -p ./certs/oficialfloov.com.br
+  chmod -R 777 ./certs
+fi
+
 chmod -R 777 ./storage
 docker compose --profile prod -f docker-compose.yaml up -d
